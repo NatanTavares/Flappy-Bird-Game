04be6e05ff5895b19c68f10612c2c9e1817e12ad
Natan Tavares <nattantavares.s15@gmail.com>
2020-07-19 17:03:00 -0300

    Change sreen

--- game.js
+++ game.js
@@ -83,17 +83,65 @@ const flappyBird = {
   }
 }
 
+const messageGetReady = {
+  sX: 134, sY: 0,
+  w: 174, h: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
+  draw() {
+    context.drawImage(
+      sprites,
+      messageGetReady.sX, messageGetReady.sY,
+      messageGetReady.w , messageGetReady.h,
+      messageGetReady.x, messageGetReady.y,
+      messageGetReady.w, messageGetReady.h    
+    );
+  }
+} 
+
+let activeScreen = {};
+
+function changeScreen(newScreen) {
+  activeScreen = newScreen;
+}
+
+const screens = {
+  init: {
+    draw() {
+      background.draw();
+      floor.draw();
+      flappyBird.draw();
+      messageGetReady.draw();
+    },
+    click() {
+      changeScreen(screens.game);
+    },
+    refresh() {
+
+    }
+  }
+};
+
+screens.game = {
+  draw() {
+    background.draw();
+    floor.draw();
+    flappyBird.draw();
+  }, refresh() {
+    flappyBird.refresh();
+  }
+};
+
 function loop() {
-  flappyBird.refresh();
-
-
-  background.draw();
-  floor.draw();
-  flappyBird.draw();
-
+  activeScreen.draw();
+  activeScreen.refresh();
   
-
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', event => {
+  if (activeScreen.click) activeScreen.click();
+});
+
+changeScreen(screens.init);
 loop();
